ec586d3fe503b92a336b8374f8ae8f420cb98cfd
Ryan13032008 <carlosryan88088@gmail.com>
2026-02-13 18:12:38 -0300

    Add files via upload

---
+++ aulaif_else.py
@@ -0,0 +1,8 @@
+entrada = input("Entrar ou sair: ").lower()
+
+if entrada == "entrar":
+    print("Você quis ficar no chat")
+elif entrada == "sair":
+    print("Você quis sair do chat")
+else: 
+    print("Digite entrar ou sair")
