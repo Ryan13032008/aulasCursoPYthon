784cb781398942791728a8938765b2b526300dc3
Ryan13032008 <carlosryan88088@gmail.com>
2026-02-16 20:19:58 -0300

    Add files via upload

---
+++ F_Strings.py
@@ -0,0 +1,7 @@
+var = "abc"
+print(f'{var}')          
+print(f'{var: >10}')
+print(f'{var: <10}.')
+print(f'{101.2988216360854:.1f}')
+print(f'O hexadecimal de 150 é {1500:08x}')
+print(f'{var!r}')
